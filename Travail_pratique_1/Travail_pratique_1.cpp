802063b16f80938ffdc71cbf1b167db8febe2720
Fairl <Fairl@MAIN>
2021-02-27 13:26:29 -0500

    !

--- Travail_pratique_1/Travail_pratique_1.cpp
+++ Travail_pratique_1/Travail_pratique_1.cpp
@@ -14,19 +14,20 @@ void ajouterTigre(Animaux* animaux[250]);
 void ajouterSinge(Animaux* animaux[250]);
 void ajouterRhino(Animaux* animaux[250]);
 void bonusEffacer(Animaux* animaux[250]);
-static int i = 5;
+static int i = 0;
 
 int main()
 {
+	
 	int choix;
 
 	Animaux* animaux[250];
 
-	animaux[0] = new Tigre("Alice", 120.8);
+	/*animaux[0] = new Tigre("Alice", 120.8);
 	animaux[1] = new Singe("Bob", 10.5, true);
 	animaux[2] = new Singe("Monke", 8.7, false);
 	animaux[3] = new Rhinoceros("Eve", 1812.3, 1000);
-	animaux[4] = new Rhinoceros("Horny", 2021.5, 6000);
+	animaux[4] = new Rhinoceros("Horny", 2021.5, 6000);*/
 
 	do {
 
